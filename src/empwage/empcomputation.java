7c51f4590bce888f6af3d9c0e8d8ff76972b875c
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 17:21:23 +0530

    employee present or absent

---
+++ src/empwage/empcomputation.java
@@ -0,0 +1,23 @@
+package empwage;
+
+public class empcomputation {
+
+public static void main(String[] args){
+		
+		int isFullTime=1;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present");
+			
+		}
+		else
+		{
+			System.out.println("Employee is Abscent");
+		}
+		
+
+	}
+
+}
