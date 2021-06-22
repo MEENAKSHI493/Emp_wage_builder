9d3bfd3215757aec511f3d34294447259a7bad4f
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-22 18:19:55 +0530

    daily_wage

--- src/empwage/empcomputation.java
+++ src/empwage/empcomputation.java
@@ -2,22 +2,26 @@ package empwage;
 
 public class empcomputation {
 
-public static void main(String[] args){
+public static void main(String[] args)
+{
+	int isFullTime=1;
+	int WagePerHr=20;
+	int FullDayHr=8;
+	
+	double empCheck = Math.floor(Math.random()*10)%2;
+	if(empCheck == isFullTime)
+	{
+		System.out.println("Employee is Present");
+		int DailyEmpWage=WagePerHr*FullDayHr;
+		System.out.println("DailyEmpWage::"+DailyEmpWage);
 		
-		int isFullTime=1;
-		
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if(empCheck == isFullTime)
-		{
-			System.out.println("Employee is Present");
-			
-		}
-		else
-		{
-			System.out.println("Employee is Abscent");
-		}
-		
-
 	}
+	else
+	{
+		System.out.println("Employee is Abscent");
+	}
+	
+}
 
 }
+		
